b1e2cbdb757e7dd6bb85598e227b2b3c8bdca556
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2025-12-23 19:02:40 +0530

    Add files via upload

---
+++ Parallellogram_pattern--.py
@@ -0,0 +1,18 @@
+rows = int(input("Enter rows:"))
+cols = int(input("Enter Cols:"))
+
+for i in range(0, rows):
+    for j in range(1, i+1):
+        print(" ", end="")
+    for j in range(0, cols):
+        print("*", end="")
+    print()
+    
+#input: Enter rows:5
+#       Enter Cols:5
+#output: 
+#   *****
+#    *****
+#     *****
+#      *****
+#       *****
